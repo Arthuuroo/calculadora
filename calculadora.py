980e1603823f58611f468c2328cb3c9e557f67f1
Arthuuroo <arthurbga2@gmail.com>
2025-08-20 21:07:20 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,12 @@
+numero1 = int(input("digite o primeiro numero"))
+numero2 = int(input("digite o segundo numero"))
+               
+soma = numero1 + numero2
+subtracao = numero1 - numero2
+multiplicacao = numero1 * numero2
+divisao = numero1 / numero2
+
+print("soma do valor:",soma)
+print("subtracao do valor:",subtracao)
+print("multiplicacao do valor:",multiplicacao)
+print("divisao do valor:",divisao)
